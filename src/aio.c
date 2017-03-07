94c40430ea5002173e4569d3d43c61c06f2146be
Patrick Mooney <pmooney@pfmooney.com>
2017-03-07 16:22:16 +0000

    Fix copyright and license headers per eng.git

--- src/aio.c
+++ src/aio.c
@@ -1,5 +1,11 @@
 /*
- * Copyright 2017 Joyent, Inc.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+/*
+ * Copyright (c) 2017, Joyent, Inc.
  */
 
 #include <unistd.h>
